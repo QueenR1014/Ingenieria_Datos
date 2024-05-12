ae22bd7471802d7853b0bd05474ad22e971bd170
DanielM13031 <54962223+DanielM13031@users.noreply.github.com>
2024-05-11 19:50:26 -0500

    Add files via upload

---
+++ Carga_masiva.sql
@@ -0,0 +1,15 @@
+copy public.Entidad from 'C:\Users\promaster13031\Desktop\DATOS\Entidad.csv' with csv header;
+
+
+copy public.Identificacion from 'C:\Users\promaster13031\Desktop\DATOS\Identificacion' with csv header;
+
+
+copy public.Negocio from 'C:\Users\promaster13031\Desktop\DATOS\Jerarquia' with csv header;
+
+
+copy public.Subtipo from 'C:\Users\promaster13031\Desktop\DATOS\Negocio' with csv header;
+
+
+copy public.Rentabilidad from 'C:\Users\promaster13031\Desktop\DATOS\Rentabilidad' with csv header;
+
+copy public.Rentabilidad from 'C:\Users\promaster13031\Desktop\DATOS\Subtipo' with csv header;
