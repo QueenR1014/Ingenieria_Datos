d9699656c0f5eb3bf04c0fed8c5831631468ed59
QueenR1014 <juanreinareyes@hotmail.com>
2024-05-21 21:27:22 -0500

    gráfica creada

--- Final/test_connection_dash.py
+++ Final/test_connection_dash.py
@@ -15,11 +15,27 @@ host = "localhost" #servidor local
 
 conn = psy2.connect(dbname = dbname, user = user, password=password, host = host)
 
-sql_query = "Select * from Negocio;" #query de testeo
+sql_query = "SELECT n.cod_negocio, n.Nombre_negocio, r.rentab_año FROM rentabilidad r JOIN entidad e ON r.cod_transaccion = e.cod_transaccion JOIN negocio n ON e.cod_negocio = n.cod_negocio;;" #query de testeo
 
 df = pd.read_sql(sql_query,conn) #referencia el sql query a la conexión
 
-print(df[['cod_negocio','nombre_negocio']].iloc[0:9])
-"""
+#print(df[['cod_negocio','nombre_negocio']].iloc[0:9])
+
+app.layout = html.Div([
+    html.H1('Rentabilidad Anual Negocio', style = {
+        'text-align':'center'
+    }),
+    dcc.Graph(
+        id = 'rentabilidad-anual',
+        figure = px.bar(df[['cod_negocio','rentab_año']].iloc[0:9], 
+                x = 'cod_negocio', y = 'rentab_año', title = 'Relación Negocio-Rentabilidad').update_layout(
+                    xaxis_title = 'Código del Negocio',
+                    yaxis_title = 'Rentabilidad Anual'
+                )
+    )
+])
+
+conn.close()
+
 if __name__ == '__main__':
-    app.run_server(port=8085)"""
+    app.run_server(port=8085)
